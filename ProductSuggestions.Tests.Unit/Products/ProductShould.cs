936b6e6b7b5cac35668a2ca39a5d1454a5a3bf90
Daniel Leach <1204887+leachdaniel@users.noreply.github.com>
2020-10-12 17:20:59 -0500

    add unit tests

---
+++ ProductSuggestions.Tests.Unit/Products/ProductShould.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductSuggestions.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductSuggestions.Tests.Unit.Products
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShowNotAvailableWhenCancelled()
+        {
+            var product = new Product(1, "name", "cat", 1.0m, 1, true);
+
+            Assert.IsFalse(product.Available);
+        }
+
+        [TestMethod]
+        public void ShowNotAvailableWhenNoQuantity()
+        {
+            var product = new Product(1, "name", "cat", 1.0m, 0, false);
+
+            Assert.IsFalse(product.Available);
+        }
+    }
+}
